4e2819a8faa521116209030b21ecd4651462e4cc
leonardonadi@gmail.com <Mutley@1#2Dist@1#6>
2022-05-14 17:48:34 -0300

    7. 2.3.2. Testando a aplicação como um usuário real - extra credit 2.2

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -27,7 +27,7 @@ describe('Hacker Stories', () => {
     // TODO: Find a way to test it out.
     it.skip('shows the right data for all rendered stories', () => { })
 
-    it.only('shows 20 stories, then the next 20 after clicking "More"', () => {
+    it('shows 20 stories, then the next 20 after clicking "More"', () => {
       cy.intercept({
         method: 'GET',
         pathname: '**/search',
@@ -84,15 +84,24 @@ describe('Hacker Stories', () => {
     const newTerm = 'Cypress'
 
     beforeEach(() => {
+      cy.intercept({
+        method: 'GET',
+        pathname: '**/search',
+        query: {
+          query: newTerm,
+          page: '0'
+        }
+      }).as('getNewTermStories')
       cy.get('#search')
         .clear()
     })
 
-    it('types and hits ENTER', () => {
+    it.only('types and hits ENTER', () => {
+     
       cy.get('#search')
         .type(`${newTerm}{enter}`)
 
-      cy.assertLoadingIsShownAndHidden()
+      cy.wait('@getNewTermStories')
 
       cy.get('.item').should('have.length', 20)
       cy.get('.item')
@@ -117,7 +126,7 @@ describe('Hacker Stories', () => {
       cy.get(`button:contains(${initialTerm})`)
         .should('be.visible')
     })
-
+    
     context('Last searches', () => {
       it('searches via the last searched term', () => {
         cy.get('#search')

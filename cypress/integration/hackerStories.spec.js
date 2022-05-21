5d164d618b35337d5502981378d4c4edae15fbb5
leonardonadi@gmail.com <Mutley@1#2Dist@1#6>
2022-05-21 19:17:08 -0300

    16. 4.1.1. Testando o frontend independente da API - extra credit 1.1

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -1,6 +1,7 @@
 describe('Hacker Stories', () => {
   const initialTerm = 'React'
   const newTerm = 'Cypress'
+
   context('Hitting the real API', () => {
 
     beforeEach(() => {
@@ -74,152 +75,170 @@ describe('Hacker Stories', () => {
     
   });
 
-  context('List of stories', () => {
-    // Since the API is external,
-    // I can't control what it will provide to the frontend,
-    // and so, how can I assert on the data?
-    // This is why this test is being skipped.
-    // TODO: Find a way to test it out.
-    it.skip('shows the right data for all rendered stories', () => { })
-
-    // it('shows 20 stories, then the next 20 after clicking "More"', () => {
-    //   cy.intercept({
-    //     method: 'GET',
-    //     pathname: '**/search',
-    //     query: {
-    //       query: initialTerm,
-    //       page: '1'
-    //     }
-    //   }).as('getNextStories')
-
-    //   cy.get('.item').should('have.length', 20)
-
-    //   cy.contains('More').click()
-
-    //   cy.wait('@getNextStories')
-
-    //   cy.get('.item').should('have.length', 40)
-    // })
-
-    // it('searches via the last searched term', () => {
-    //   cy.get('#search')
-    //     .type(`${newTerm}{enter}`)
-
-    //   cy.wait('@getNewTermStories')
-
-    //   cy.get(`button:contains(${initialTerm})`)
-    //     .should('be.visible')
-    //     .click()
-
-    //   cy.wait('@getStories')
-
-    //   cy.get('.item').should('have.length', 20)
-    //   cy.get('.item')
-    //     .first()
-    //     .should('contain', initialTerm)
-    //   cy.get(`button:contains(${newTerm})`)
-    //     .should('be.visible')
-    // })
-
-    it('shows only nineteen stories after dimissing the first story', () => {
-      cy.get('.button-small')
-        .first()
-        .click()
-
-      cy.get('.item').should('have.length', 19)
-    })
-
-    // Since the API is external,
-    // I can't control what it will provide to the frontend,
-    // and so, how can I test ordering?
-    // This is why these tests are being skipped.
-    // TODO: Find a way to test them out.
-    context.skip('Order by', () => {
-      it('orders by title', () => { })
-
-      it('orders by author', () => { })
-
-      it('orders by comments', () => { })
-
-      it('orders by points', () => { })
-    })
-  })
-
-  context('Search', () => {
+  context.only('Mocking the API', () => {
     beforeEach(() => {
-      cy.intercept({
-        method: 'GET',
-        pathname: '**/search',
-        query: {
-          query: newTerm,
-          page: '0'
-        }
-      }).as('getNewTermStories')
-      cy.get('#search')
-        .clear()
+      cy.intercept(
+        'GET',
+        `**/search?query=${initialTerm}&page=0`,
+        {fixture:'stories'}
+      ).as('getStories')
+
+      cy.visit('/')
+      cy.wait('@getStories')
     })
-
-    it('types and hits ENTER', () => {
-
-      cy.get('#search')
-        .type(`${newTerm}{enter}`)
-
-      cy.wait('@getNewTermStories')
-
-      cy.get('.item').should('have.length', 20)
-      cy.get('.item')
-        .first()
-        .should('contain', newTerm)
-      cy.get(`button:contains(${initialTerm})`)
+  
+    it('shows the footer', () => {
+      cy.get('footer')
         .should('be.visible')
+        .and('contain', 'Icons made by Freepik from www.flaticon.com')
     })
-
-    it('types and clicks the submit button', () => {
-      cy.get('#search')
-        .type(newTerm)
-      cy.contains('Submit')
-        .click()
-
-      cy.wait('@getNewTermStories')
-
-      cy.get('.item').should('have.length', 20)
-      cy.get('.item')
-        .first()
-        .should('contain', newTerm)
-      cy.get(`button:contains(${initialTerm})`)
-        .should('be.visible')
-    })
-
-    it('types and submits the form directly', () => {
-      cy.get('form input[type="text"]')
-        .should('be.visible')
-        .clear()
-        .type('cypress')
-      cy.get('form').submit()
-
-      // Assertion here
-    })
-
-    context('Last searches', () => {
-      it('shows a max of 5 buttons for the last searched terms', () => {
-        const faker = require('faker')
-
-        cy.intercept({
-          method: 'GET',
-          pathname: '**/search**'
-        }).as('getRandomStories')
-
-        Cypress._.times(6, () => {
-          cy.get('#search')
-            .clear()
-            .type(`${faker.random.word()}{enter}`)
-          cy.wait('@getRandomStories')
-        })
-        cy.get('.last-searches button')
-          .should('have.length', 5)
+  
+    context('List of stories', () => {
+      // Since the API is external,
+      // I can't control what it will provide to the frontend,
+      // and so, how can I assert on the data?
+      // This is why this test is being skipped.
+      // TODO: Find a way to test it out.
+      it.skip('shows the right data for all rendered stories', () => { })
+  
+      // it('shows 20 stories, then the next 20 after clicking "More"', () => {
+      //   cy.intercept({
+      //     method: 'GET',
+      //     pathname: '**/search',
+      //     query: {
+      //       query: initialTerm,
+      //       page: '1'
+      //     }
+      //   }).as('getNextStories')
+  
+      //   cy.get('.item').should('have.length', 20)
+  
+      //   cy.contains('More').click()
+  
+      //   cy.wait('@getNextStories')
+  
+      //   cy.get('.item').should('have.length', 40)
+      // })
+  
+      // it('searches via the last searched term', () => {
+      //   cy.get('#search')
+      //     .type(`${newTerm}{enter}`)
+  
+      //   cy.wait('@getNewTermStories')
+  
+      //   cy.get(`button:contains(${initialTerm})`)
+      //     .should('be.visible')
+      //     .click()
+  
+      //   cy.wait('@getStories')
+  
+      //   cy.get('.item').should('have.length', 20)
+      //   cy.get('.item')
+      //     .first()
+      //     .should('contain', initialTerm)
+      //   cy.get(`button:contains(${newTerm})`)
+      //     .should('be.visible')
+      // })
+  
+      it.only('shows on story after dimissing the first one', () => {
+        cy.get('.button-small')
+          .first()
+          .click()
+        cy.get('.item').should('have.length', 1)
+      })
+  
+      // Since the API is external,
+      // I can't control what it will provide to the frontend,
+      // and so, how can I test ordering?
+      // This is why these tests are being skipped.
+      // TODO: Find a way to test them out.
+      context.skip('Order by', () => {
+        it('orders by title', () => { })
+  
+        it('orders by author', () => { })
+  
+        it('orders by comments', () => { })
+  
+        it('orders by points', () => { })
       })
     })
-  })
+  
+    context('Search', () => {
+      beforeEach(() => {
+        cy.intercept({
+          method: 'GET',
+          pathname: '**/search',
+          query: {
+            query: newTerm,
+            page: '0'
+          }
+        }).as('getNewTermStories')
+        cy.get('#search')
+          .clear()
+      })
+  
+      it('types and hits ENTER', () => {
+  
+        cy.get('#search')
+          .type(`${newTerm}{enter}`)
+  
+        cy.wait('@getNewTermStories')
+  
+        cy.get('.item').should('have.length', 20)
+        cy.get('.item')
+          .first()
+          .should('contain', newTerm)
+        cy.get(`button:contains(${initialTerm})`)
+          .should('be.visible')
+      })
+  
+      it('types and clicks the submit button', () => {
+        cy.get('#search')
+          .type(newTerm)
+        cy.contains('Submit')
+          .click()
+  
+        cy.wait('@getNewTermStories')
+  
+        cy.get('.item').should('have.length', 20)
+        cy.get('.item')
+          .first()
+          .should('contain', newTerm)
+        cy.get(`button:contains(${initialTerm})`)
+          .should('be.visible')
+      })
+  
+      it('types and submits the form directly', () => {
+        cy.get('form input[type="text"]')
+          .should('be.visible')
+          .clear()
+          .type('cypress')
+        cy.get('form').submit()
+  
+        // Assertion here
+      })
+  
+      context('Last searches', () => {
+        it('shows a max of 5 buttons for the last searched terms', () => {
+          const faker = require('faker')
+  
+          cy.intercept({
+            method: 'GET',
+            pathname: '**/search**'
+          }).as('getRandomStories')
+  
+          Cypress._.times(6, () => {
+            cy.get('#search')
+              .clear()
+              .type(`${faker.random.word()}{enter}`)
+            cy.wait('@getRandomStories')
+          })
+          cy.get('.last-searches button')
+            .should('have.length', 5)
+        })
+      })
+    })
+  });
 })
 
 context('Errors', () => {

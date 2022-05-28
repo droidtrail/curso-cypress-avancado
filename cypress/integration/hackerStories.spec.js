55dcf6c9f21a7360fde060efddb63b7d76daf4d5
leonardonadi@gmail.com <Mutley@1#2Dist@1#6>
2022-05-28 19:06:26 -0300

    20. 4.2.1. Testando o frontend independente da API - extra credit 2.1

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -75,7 +75,7 @@ describe('Hacker Stories', () => {
 
   });
 
-  context.only('Mocking the API', () => {
+  context('Mocking the API', () => {
     context('Footer and list of stories', () => {
       beforeEach(() => {
         cy.intercept(
@@ -95,12 +95,27 @@ describe('Hacker Stories', () => {
       })
 
       context('List of stories', () => {
-        // Since the API is external,
-        // I can't control what it will provide to the frontend,
-        // and so, how can I assert on the data?
-        // This is why this test is being skipped.
-        // TODO: Find a way to test it out.
-        it.skip('shows the right data for all rendered stories', () => { })
+        it.only('shows the right data for all rendered stories', () => {
+
+          const stories = require('../fixtures/stories.json')
+          cy.get('.item')
+            .first()
+            .should('contain', stories.hits[0].title)
+            .and('contain', stories.hits[0].author)
+            .and('contain', stories.hits[0].num_comments)
+            .and('contain', stories.hits[0].points)
+          cy.get(`.item a:contains(${stories.hits[0].title})`)
+            .should('have.attr', 'href', stories.hits[0].url)
+          
+            cy.get('.item')
+            .last()
+            .should('contain', stories.hits[1].title)
+            .and('contain', stories.hits[1].author)
+            .and('contain', stories.hits[1].num_comments)
+            .and('contain', stories.hits[1].points)
+          cy.get(`.item a:contains(${stories.hits[1].title})`)
+            .should('have.attr', 'href', stories.hits[1].url)
+        })
 
         // it('shows 20 stories, then the next 20 after clicking "More"', () => {
         //   cy.intercept({
@@ -187,7 +202,7 @@ describe('Hacker Stories', () => {
       })
 
       it('types and hits ENTER', () => {
-        
+
         cy.get('#search')
           .clear()
           .type(`${newTerm}{enter}`)
@@ -222,14 +237,14 @@ describe('Hacker Stories', () => {
         // Assertion here
       })
 
-      context.only('Last searches', () => {
+      context('Last searches', () => {
         it('shows a max of 5 buttons for the last searched terms', () => {
           const faker = require('faker')
 
           cy.intercept(
             'GET',
             '**/search**',
-            {fixture:'empty'}
+            { fixture: 'empty' }
           ).as('getRandomStories')
 
           Cypress._.times(6, () => {

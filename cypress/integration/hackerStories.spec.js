5a852224e9ffabfa86d9f0e5a2314269bb6f6155
leonardonadi@gmail.com <Mutley@1#2Dist@1#6>
2022-06-05 12:46:34 -0300

    33. 9.1. Modificando o relatório dos testes - exercício

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -408,7 +408,7 @@ context('Errors', () => {
   })
 })
 
-it.only('shows a "Loading ..." state before showing the results', () => {
+it('shows a "Loading ..." state before showing the results', () => {
   cy.intercept(
     'GET',
     '**/search**',
